21beefa7026f0a35fd6f2e40ae99789f9a70be5d
mmbalbas <mmbalbasgolf@gmail.com>
2024-02-09 01:19:39 +0100

    Creación de modelos y componentes

---
+++ app/Livewire/ExpenseManager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ExpenseManager extends Component
+{
+    public function render()
+    {
+        return view('livewire.expense-manager');
+    }
+}
---
+++ app/Livewire/IncomeManager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class IncomeManager extends Component
+{
+    public function render()
+    {
+        return view('livewire.income-manager');
+    }
+}
