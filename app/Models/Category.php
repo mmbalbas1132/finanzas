7f784dca3aa00927fcaa5567b4327e4cec76bc51
mmbalbas <mmbalbasgolf@gmail.com>
2024-02-09 01:37:17 +0100

    Modelos cubiertos

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    // Asumiendo que 'name' es un campo en tu tabla de categorías.
+    // Agrega más campos al array $fillable según sea necesario.
+    protected $fillable = ['name'];
+
+    public function expenses()
+    {
+        return $this->hasMany(Income::class);
+    }
+
+    public function incomes()
+    {
+        return $this->hasMany(Income::class);
+    }
 }
--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Expense extends Model
 {
     use HasFactory;
+
+    // Especifica los campos que deseas sean asignables masivamente.
+    protected $fillable = ['amount', 'category_id', 'date', 'description'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
